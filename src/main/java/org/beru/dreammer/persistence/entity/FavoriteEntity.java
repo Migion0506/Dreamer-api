80262b834177ea9abe8d993812cf0af7704aaac4
MichaelJQC <michael.chavezjq@gmail.com>
2024-02-26 13:07:44 -0500

    Added ID field to the FavoriteEntity

--- src/main/java/org/beru/dreammer/persistence/entity/FavoriteEntity.java
+++ src/main/java/org/beru/dreammer/persistence/entity/FavoriteEntity.java
@@ -1,17 +1,13 @@
 package org.beru.dreammer.persistence.entity;
 
+import jakarta.persistence.*;
 import org.beru.dreammer.persistence.audit.AuditableDreamEntity;
+import org.beru.dreammer.persistence.entity.id.UserDreamId;
+import org.hibernate.annotations.UuidGenerator;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -23,7 +19,11 @@ import lombok.ToString;
 @ToString
 @EntityListeners({AuditingEntityListener.class})
 public class FavoriteEntity extends AuditableDreamEntity {
+
     @Id
+    @UuidGenerator
+    private String id;
+
     @Column(nullable = false, length = 50)
     private String dream;
 
